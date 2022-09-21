c027ed82961e099d57268a11de7112d542fc3de0
Mattazz <matthewazada101@gmail.com>
2022-09-21 16:18:29 +0800

    added catch errors for wrong user input

--- main.py
+++ main.py
@@ -28,12 +28,29 @@ def showUserMenu(): # Shows selection of city or barangay
 
 
 def searchFor(df, userAnswer): # Search # UserAnswer 1 = Metro Manila, 2 = Province
-    userMenu = input('Select: ')
-    if userAnswer == '1':
-        col1Name = 'City'
-    else:
-        col1Name = 'Province'
-    if userMenu == '1': # Search for City list zip codes
+    
+    while True:
+        try:
+            userMenu = int(input('Select: '))
+            if userMenu == 1 or userMenu ==2:
+                break
+            else:
+                print("Not a valid number, please try again")
+        except ValueError:
+            print("Not a valid answer, please try again")
+    
+    while True:
+        try:
+            if userAnswer == 1:
+                col1Name = 'City'
+                break
+            else:
+                col1Name = 'Province'
+                break
+        except ValueError:
+            print("ValueError")
+            
+    if userMenu == 1: # Search for City list zip codes
         cityPick = input('Input City: ')
         df = df[df[col1Name].str.contains(cityPick, na=False, case=False)] # na=False removes blank passages in the file if any
         make_line()
@@ -42,7 +59,7 @@ def searchFor(df, userAnswer): # Search # UserAnswer 1 = Metro Manila, 2 = Provi
         else: 
             print(df)
         make_line()
-    elif userMenu == '2': # Search for Barangay / District direct zip codes
+    elif userMenu == 2: # Search for Barangay / District direct zip codes
         brgyPick = input('Input Barangay: ')
         df = df[df['Barangay'].str.contains(brgyPick, na=False, case=False)] # na=False removes blank passages in the file if any
         make_line()
@@ -58,8 +75,20 @@ while True:
             (1) Metro Manila
             (2) Regional
             ''')
-    userAnswer = input('Answer: ')
-    if userAnswer == '1':
+    userAnswer = 0
+    
+    while userAnswer != 1 or userAnswer != 2:
+        try:
+            userAnswer = int(input('Answer: '))
+            if userAnswer == 1 or userAnswer == 2:
+               break
+            else:
+                print("Please select within the range of the menu")
+        except ValueError:
+            print("That is not a valid answer, try again")
+        
+   
+    if userAnswer == 1:
         file_name = 'zips/MetroManilaZipCodes.xlsx'
         df = pd.read_excel(io=file_name, sheet_name='MM',) # Currently only reads the sheet 'MM' - Metro Manila
         df['ZIP Code'] = df['ZIP Code'].astype(int) # Converts excel values to INT for the zip code column
@@ -81,6 +110,7 @@ while True:
     showUserMenu()
     searchFor(df, userAnswer)
     make_line()
+    
     endProgram = input("End program?(y/n) ")
     if endProgram == 'y':
         sys.exit()
